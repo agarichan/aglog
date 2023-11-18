70ae2a54587bc96036b29969ef91fb02f44b3410
agarichan <ag4r1chan@gmail.com>
2023-11-18 15:03:52 +0900

    Bump version to 0.1.1 and update release workflow

--- src/aglog/__init__.py
+++ src/aglog/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 from .filter import MessageWordFilter, NameFilter, ProcessNameFilter, ThreadNameFilter
 from .formatter import JsonColorFormatter, JsonFormatter
