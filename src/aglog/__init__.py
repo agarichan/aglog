f2c3c3da97480b56cec7999da8ef97d251f6bfae
agarichan <ag4r1chan@gmail.com>
2023-11-18 15:09:05 +0900

    Update version number to 0.1.2

--- src/aglog/__init__.py
+++ src/aglog/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 
 from .filter import MessageWordFilter, NameFilter, ProcessNameFilter, ThreadNameFilter
 from .formatter import JsonColorFormatter, JsonFormatter
